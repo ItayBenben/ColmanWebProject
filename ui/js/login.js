df2ece0d401f8a0cc9b756e4196a14a0b37cd106
niraroet <niraroet@gmail.com>
2025-08-05 21:33:26 +0300

    frieds rework

---
+++ ui/js/login.js
@@ -0,0 +1,38 @@
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+$(document).ready(function () {
+    const loginForm = $('#login-form');
+    const messageElement = $('#login-message');
+
+    loginForm.on('submit', function (e) {
+        e.preventDefault();
+
+        const email = $('#email').val();
+        const password = $('#password').val();
+
+        $.ajax({
+            url: `http://localhost:5000/api/auth/login`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({ email, password,}),
+            xhrFields: { withCredentials: true }, // for cookies to be sent with cross-origin requests
+            success: function (data) {
+                console.log('Login successful');
+                const user_id = getCookie('id');
+                console.log('Logged in user:', getCookie('username'));
+                window.location.href = 'feed.html';
+            },
+            error: function (jqXHR) {
+                const errorMessage = jqXHR.responseJSON?.msg || 'An unknown error occurred';
+                console.error('Error:', errorMessage);
+                messageElement.text('Error: ' + errorMessage).css('color', 'red');
+            }
+        });
+    });
+});
