aeba9bef894cab134092ba558f4c2e4a1d8fd863
Liron Drory <liron4635527@gmail.com>
2025-08-06 00:07:38 +0300

    remove some files

--- routes/feedRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { auth } from '../middleware/auth.js';
-import { getFeed } from '../controllers/feedController.js';
-
-const router = express.Router();
-
-router.get('/', auth, getFeed);
-
-export default router; 
