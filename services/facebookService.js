13ba8f8c8e58df654072e1ae94ae968ee0d1ab02
Liron Drory <liron4635527@gmail.com>
2025-08-06 00:07:38 +0300

    remove some files

--- services/facebookService.js
+++
@@ -1,4 +0,0 @@
-export const sendWelcomeFacebook = (user) => {
-  // Simulate sending a welcome message to Facebook
-  console.log(`Sent welcome message to Facebook for user: ${user.username}`);
-}; 
