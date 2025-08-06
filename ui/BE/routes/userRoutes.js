8385413fa933be27ea2b293101b67735cbcc2846
Liron Drory <liron4635527@gmail.com>
2025-08-06 17:04:02 +0300

    made many things

---
+++ ui/BE/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { auth } from '../middleware/auth.js';
+import { getUser, updateUser, deleteUser, searchUsers, addFriend, approveFriendRequest, blockUser } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.get('/:id', auth, getUser);
+router.put('/:id', auth, updateUser);
+router.delete('/:id', auth, deleteUser);
+router.get('/search', auth, searchUsers);
+router.post('/add-friend', auth, addFriend);
+router.post('/approve-friend', auth, approveFriendRequest);
+router.post('/block', auth, blockUser);
+
+export default router; 
